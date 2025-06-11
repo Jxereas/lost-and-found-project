6ddb177f24d73d1a52a0209e7988c89ce90d64b4
John Xereas <dcf9rp@virginia.edu>
2025-06-11 03:14:08 -0400

    Fixed: collapseable sidebar animations and added svg button

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
-import '../styles/Sidebar.css';
-import {useNavigate} from 'react-router-dom'
+import "../styles/Sidebar.css";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-function Sidebar({collapsed, setCollapsed, setAdminInfo}) {
-    const navigate = useNavigate()
+function Sidebar({ collapsed, setCollapsed, setAdminInfo }) {
+  const navigate = useNavigate();
 
-    const handleLogout = async () => {
-        setAdminInfo(null)
-        navigate('/')
-    }
+  const handleLogout = async () => {
+    setAdminInfo(null);
+    navigate("/");
+  };
 
-    return (
-    <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
+  return (
+    <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
       <div className="sidebar-content">
-        <button className="collapse-toggle" onClick={() => setCollapsed(!collapsed)}>
-          {collapsed ? '→' : '←'}
-        </button>
-
-        <h2 className="sidebar-title">Dashboard</h2>
-
+        <div className="sidebar-header">
+          <h2 className="sidebar-title">Dashboard</h2>
+          <button
+            className="collapse-toggle"
+            onClick={() => setCollapsed(!collapsed)}
+          >
+            <FontAwesomeIcon
+              className={`collapse-arrow ${collapsed ? "rotated" : ""}`}
+              icon={faArrowLeft}
+            />
+          </button>
+        </div>
         <ul className="sidebar-nav">
           <li>Home</li>
           <li>Add Lost Item</li>
@@ -28,9 +35,11 @@ function Sidebar({collapsed, setCollapsed, setAdminInfo}) {
         </ul>
       </div>
 
-      <button className="logout-button" onClick={handleLogout}>Logout</button>
+      <button className="logout-button" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
-    )
+  );
 }
 
 export default Sidebar;
