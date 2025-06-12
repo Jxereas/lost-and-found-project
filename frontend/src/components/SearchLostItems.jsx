4646d94bfe52156c452f269f6f2cfc96bf947d32
John Xereas <dcf9rp@virginia.edu>
2025-06-12 16:42:15 -0400

    Added: navigation to search result page from search

--- frontend/src/components/SearchLostItems.jsx
+++ frontend/src/components/SearchLostItems.jsx
@@ -65,105 +65,112 @@ function SearchLostItems() {
         })
             .then((res) => res.json())
             .then((data) => {
-                navigate("/search-results", { state: { results: data.results } });
+                navigate("/lost-items-search-results", { state: { results: data.results } });
             })
             .catch((error) => console.error("Search error:", error));
     };
 
     return (
         <div className="search-container">
-            <h2 className="search-title">Search Lost Items</h2>
-            <div className="group-container">
-                <div className="search-group">
-                    <h3 className="search-group-title">Item</h3>
-                    <input
-                        type="text"
-                        placeholder="Item Name"
-                        value={itemName}
-                        onChange={(e) => setItemName(e.target.value)}
-                        className=""
-                    />
-                    <Select
-                        components={animatedComponents}
-                        options={locationOptions}
-                        classNamePrefix="react-select"
-                        className="react-select-container"
-                        value={selectedLocation}
-                        onChange={setSelectedLocation}
-                        placeholder="Select a location"
-                    />
-                    <Select
-                        closeMenuOnSelect={false}
-                        components={animatedComponents}
-                        isMulti
-                        options={tagOptions}
-                        classNamePrefix="react-select"
-                        className="react-select-container"
-                        value={selectedTags}
-                        onChange={setSelectedTags}
-                        placeholder="Select tags"
-                    />
-                </div>
+            <Sidebar
+                setCollapsed={sidebarSetCollapsed}
+                collapsed={sidebarCollapsed}
+                setAdminInfo={setAdminInfo}
+            />
+            <div className={`search-main ${sidebarCollapsed ? "sidebar-collapsed": ""}`}>
+                <h2 className="search-title">Search Lost Items</h2>
+                <div className="group-container">
+                    <div className="search-group">
+                        <h3 className="search-group-title">Item</h3>
+                        <input
+                            type="text"
+                            placeholder="Item Name"
+                            value={itemName}
+                            onChange={(e) => setItemName(e.target.value)}
+                            className=""
+                        />
+                        <Select
+                            components={animatedComponents}
+                            options={locationOptions}
+                            classNamePrefix="react-select"
+                            className="react-select-container"
+                            value={selectedLocation}
+                            onChange={setSelectedLocation}
+                            placeholder="Select a location"
+                        />
+                        <Select
+                            closeMenuOnSelect={false}
+                            components={animatedComponents}
+                            isMulti
+                            options={tagOptions}
+                            classNamePrefix="react-select"
+                            className="react-select-container"
+                            value={selectedTags}
+                            onChange={setSelectedTags}
+                            placeholder="Select tags"
+                        />
+                    </div>
 
-                <div className="search-group">
-                    <h3 className="search-group-title">Reporter</h3>
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        value={reporterFirstName}
-                        onChange={(e) => setReporterFirstName(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        value={reporterLastName}
-                        onChange={(e) => setReporterLastName(e.target.value)}
-                    />
-                    <label>Reported Start Date:</label>
-                    <input
-                        type="date"
-                        value={reportedStartDate}
-                        onChange={(e) => setReportedStartDate(e.target.value)}
-                    />
-                    <label>Reported End Date:</label>
-                    <input
-                        type="date"
-                        value={reportedEndDate}
-                        onChange={(e) => setReportedEndDate(e.target.value)}
-                    />
-                </div>
+                    <div className="search-group">
+                        <h3 className="search-group-title">Reporter</h3>
+                        <input
+                            type="text"
+                            placeholder="First Name"
+                            value={reporterFirstName}
+                            onChange={(e) => setReporterFirstName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Last Name"
+                            value={reporterLastName}
+                            onChange={(e) => setReporterLastName(e.target.value)}
+                        />
+                        <label>Reported Start Date:</label>
+                        <input
+                            type="date"
+                            value={reportedStartDate}
+                            onChange={(e) => setReportedStartDate(e.target.value)}
+                        />
+                        <label>Reported End Date:</label>
+                        <input
+                            type="date"
+                            value={reportedEndDate}
+                            onChange={(e) => setReportedEndDate(e.target.value)}
+                        />
+                    </div>
 
-                <div className="search-group">
-                    <h3 className="search-group-title">Claimer</h3>
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        value={claimerFirstName}
-                        onChange={(e) => setClaimerFirstName(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        value={claimerLastName}
-                        onChange={(e) => setClaimerLastName(e.target.value)}
-                    />
-                    <label>Claimed Start Date:</label>
-                    <input
-                        type="date"
-                        value={claimedStartDate}
-                        onChange={(e) => setClaimedStartDate(e.target.value)}
-                    />
-                    <label>Claimed End Date:</label>
-                    <input
-                        type="date"
-                        value={claimedEndDate}
-                        onChange={(e) => setClaimedEndDate(e.target.value)}
-                    />
+                    <div className="search-group">
+                        <h3 className="search-group-title">Claimer</h3>
+                        <input
+                            type="text"
+                            placeholder="First Name"
+                            value={claimerFirstName}
+                            onChange={(e) => setClaimerFirstName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Last Name"
+                            value={claimerLastName}
+                            onChange={(e) => setClaimerLastName(e.target.value)}
+                        />
+                        <label>Claimed Start Date:</label>
+                        <input
+                            type="date"
+                            value={claimedStartDate}
+                            onChange={(e) => setClaimedStartDate(e.target.value)}
+                        />
+                        <label>Claimed End Date:</label>
+                        <input
+                            type="date"
+                            value={claimedEndDate}
+                            onChange={(e) => setClaimedEndDate(e.target.value)}
+                        />
+                    </div>
                 </div>
+                <button className="search-button" onClick={handleSearch}>
+                    Search
+                </button>
             </div>
-            <button className="search-button" onClick={handleSearch}>
-                Search
-            </button>
         </div>
     );
 }
